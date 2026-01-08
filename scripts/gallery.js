80c1aa8e12ccf8b229ca84fd4660136b2888a605
Neodynix Technologies <neodynixtechnologies@gmail.com>
2026-01-08 16:07:43 +0300

    Update gallery.js

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,6 +1,6 @@
 // === CONFIGURE YOUR SUPABASE DETAILS HERE ===
 const SUPABASE_URL = 'https://maotqbvahqunerrcjmnj.supabase.co';          // e.g., https://abcde.supabase.co
-const SUPABASE_ANON_KEY = 'sb_publishable_I12go3As_fRPeyVofz7ulg_29_EMPjL';            // Safe to expose with RLS enabled
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hb3RxYnZhaHF1bmVycmNqbW5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4NTUyMTMsImV4cCI6MjA4MzQzMTIxM30.ifuW5vgU8PgPRpvRlrknflrDwM_2sRsvxY8Oh5eac4M';            // Safe to expose with RLS enabled
 
 const supabase = Supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
